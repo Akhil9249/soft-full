5c066e05ee7bb148768f62909346f8b73a28f920
Akhil9249 <akhilcv430@gmail.com>
2026-08-11 11:02:44 +0530

    Something went wrong. Please try again. - solved this issue

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const apiUrl = import.meta.env.VITE_BACKEND_URL;
-const apiUrl = "http://localhost:3000"
+const apiUrl = import.meta.env.VITE_BACKEND_URL;
 
 
 export const BASE_URL = apiUrl;
